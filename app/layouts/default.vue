843eca3ff92b00c14cf141806970ca936a40f338
adhillon192 <124457853+adhillon192@users.noreply.github.com>
2025-12-25 15:38:58 -0700

    Added Vercel Analytics for now

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
 </script>
 
 <template>
   <div>
+    <Analytics />
     <UContainer class="sm:border-x border-default pt-10">
       <AppHeader :links="navLinks" />
       <slot />
